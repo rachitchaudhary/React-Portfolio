de3d77358a8a2a09180944547a77c3cedad2fe1e
rachitchaudhary <rachit5399@gmail.com>
2020-09-18 00:46:44 +0530

    Added Corousal

--- src/App.js
+++ src/App.js
@@ -1,12 +1,13 @@
 import React from 'react';
 import './App.css';
 import MyNavbar from "./components/mynavbar/mynavbar.component";
+import MyCarousal from './components/my-carousal/my-carousal.component';
 
 function App() {
   return (
     <div>
       <MyNavbar/>
-      
+      <MyCarousal/>
     </div>
   );
 }
