8d8aaa145dd16e018bdeaa8d9e972c6e64aa95e5
Rachit <rachit5399@gmail.com>
2021-05-20 16:49:06 +0530

    Updates

---
+++ precache-manifest.952ed4d4e5c662e2545aa66e6386aa6e.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cf5b131b895f0141ffaebb3614b9f4e3",
+    "url": "/React-Portfolio/index.html"
+  },
+  {
+    "revision": "1b081d667b7bfa8260ec",
+    "url": "/React-Portfolio/static/css/main.89add09e.chunk.css"
+  },
+  {
+    "revision": "b3b67079c07f8e891c61",
+    "url": "/React-Portfolio/static/js/2.1fc240e3.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/React-Portfolio/static/js/2.1fc240e3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1b081d667b7bfa8260ec",
+    "url": "/React-Portfolio/static/js/main.ebc4e162.chunk.js"
+  },
+  {
+    "revision": "524bc16ed1f7508f1d31",
+    "url": "/React-Portfolio/static/js/runtime-main.ce309103.js"
+  },
+  {
+    "revision": "8bce7aef886c3872c218e0e6ef8bdccb",
+    "url": "/React-Portfolio/static/media/slide1.8bce7aef.webp"
+  },
+  {
+    "revision": "7256f309520276be70acd97720610bc6",
+    "url": "/React-Portfolio/static/media/slide2.7256f309.webp"
+  }
+]);
