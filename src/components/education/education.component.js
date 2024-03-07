8c4f8d0ab135cbdffda2a48849000e90cab5c446
Rachit Chaudhary <rachit5399@gmail.com>
2024-03-08 01:54:48 +0530

    updating work experience

--- src/components/education/education.component.js
+++ src/components/education/education.component.js
@@ -26,25 +26,25 @@ class Education extends Component {
                                                     <h3>Goldman Sachs</h3> <h5>Associate</h5> <span className = "year">01/2022 - Present</span>
                                                     <br />
                                                     <ul>
-                                                        <li> Led the automated KYCasaservicedevelopmentforclientsonboardedbyacquiredcompanies. Came up with an
-                                                             innovative design</li>
-                                                        <li> Designed and implemented Front-end and back-end changes for our Regulator flow to capture the clients regulators and their validity</li>
+                                                        <li> Led the automated KYC as a service development for clients onboarded by acquired companies to read data from the database to complete the KYC of critical clients as past of a regulatory commitment</li>
+                                                        <li> Designed and implemented Frontend and Backend for our Regulator flow to capture the clients regulators and their validity</li>
                                                         <li> Developed the Maintenance and Monitoring flow for triggering the client’s Rolling review KYC based on risk changes</li>
                                                         <li> Designed and implemented a Kafka based event service to reduce the DB load</li>
                                                         <li> Worked on several performance enhancements like DB Archival, Elastic Search and DB query optimization to tackle the latency issue when integrated with a new business line</li>
                                                         <li> Worked on several bug fixes and enhancements for our Client Onboarding services used for capturing client's KYC data based on Regulatory Rules</li>
                                                         <li> Migrated Rule implementations from legacy to a SpringBoot based Micro-services architecture under tight deadlines for the Rules As A Service product development under KYC as a Service roadmap</li>
+                                                        <li> Tech Stack: Java, React, Spring Boot, Jersey, Apache Kafka, Kubernetes, MongoDB, Sybase, Git, Elasticsearch</li>
                                                     </ul>
                                                 </p>
                                                 <p>
                                                     <h3>Amdocs</h3> <h5>Software Engineer Associate</h5> <span className = "year">08/2020 - 01/2022</span>
                                                     <br />
                                                     <ul>
-                                                        <li> Worked as a full stack developer for a Satellite Communication client to customize the company's inventory management product for client's needs</li>
-                                                        <li> Developed multiple Restful web services on top of our Telecom inventory management product to customize it for satellite communication and integrate with the client's UI</li>
+                                                        <li> Developed multiple Restful web services on top of our Telecommunication Inventory Management product to customize it for satellite communication and integrate with the client's UI</li>
                                                         <li> Developed a java application to automate data modelling process which was done using an internal tool earlier leading to reduction of developer's working time by 90%</li>
                                                         <li> Worked on several PL/SQL scripts called by different services for adding and modifying Telecom inventory in the database</li>
                                                         <li> Mentored & on-boarded freshers in the team to up-skill them to the best of engineering practices in the industry</li>
+                                                        <li> Tech Stack: Java, Spring Boot, PL/SQL, Oracle DB, Maven, Perforce</li>
                                                     </ul>
                                                 </p>
                                                 <p>
@@ -53,7 +53,7 @@ class Education extends Component {
                                                     <ul>
                                                         <li> Worked on the B2B bill generation flow to change the format and added additional attributes to comply with TRAI mandate</li>
                                                         <li> Developed new SQL procedures for the attributes generation and persistence</li>
-                                                        <li> Worked on React based UI for B2B bills</li>
+                                                        <li> Tech Stack: Java, Spring Boot, SQL</li>
                                                     </ul>
                                                 </p>
                                                 <p>
